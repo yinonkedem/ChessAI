47c697641f60ac16c80b24712f1552e549704e5a
yinonkedem <96231578+yinonkedem@users.noreply.github.com>
2025-07-30 11:26:08 +0300

    change db to SQLite

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,17 +1,10 @@
-from contextlib import contextmanager
-
 from sqlmodel import create_engine, Session
-
 from .core.config import settings
 
-engine = create_engine(settings.db_url, echo=False, pool_pre_ping=True)
+url = settings.db_url
+connect_args = {"check_same_thread": False} if url.startswith("sqlite") else {}
+engine = create_engine(url, echo=False, pool_pre_ping=True, connect_args=connect_args)
 
 def init_db() -> None:
-    """Call once to create all tables."""
-    from .models import SQLModel  # import late to avoid circulars
+    from .models import SQLModel
     SQLModel.metadata.create_all(engine)
-
-@contextmanager
-def get_session():
-    with Session(engine) as session:
-        yield session
