abf2d7dd0d35faf069aff0a5b132be9e34ecfd45
yinonkedem <96231578+yinonkedem@users.noreply.github.com>
2025-11-24 15:33:30 +0200

    init db

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,6 +3,10 @@ from typing import Generator
 from sqlmodel import create_engine, Session
 from .settings import DATABASE_URL
 
+import logging
+logger = logging.getLogger(__name__)
+logger.info(f"*** USING DATABASE_URL = {DATABASE_URL}")
+
 engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 
 # ✅ FastAPI dependency that YIELDS a real Session
